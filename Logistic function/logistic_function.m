ef68ca8a2cabbb5c232fd0fe8faf7d6d7b7eab84
Soerj <shaman32r@gmail.com>
2019-10-21 23:36:25 +0300

    logistic funtion

--- Logistic function/logistic_function.m
+++ Logistic function/logistic_function.m
@@ -1,7 +1,7 @@
-X = [0:0.1:6];
+X = [-10:0.5:10];
 Y = zeros(size(X));
 for i = 1:(length(X)),
-  Y(i) = 1 / (1 + exp(X(i)));
+  Y(i) = 1 / (1 + exp(-X(i)));
 end;
 plot(X, Y, 'ob', 'linestyle', "-");
 hold on;
